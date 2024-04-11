fa0e67de1d2914ce06fbc5d4bc3933166f8f710d
Falon Strada <59482978+FalonStrada@users.noreply.github.com>
2024-04-10 21:28:32 -0300

    Update SearchProduct.spec.js

--- tests/SearchProduct.spec.js
+++ tests/SearchProduct.spec.js
@@ -7,7 +7,6 @@ await page.goto('https://automationexercise.com/');
 await page.getByRole('link', {name: 'Products'}).click()
 await expect(page.getByText('All Products')).toBeVisible()
 await page.getByPlaceholder('Search Product').fill('dress')
-//await page.getByRole('button', { name: '' }).click();
 await page.locator('//button[@id="submit_search"]').click()
 
 const productDetailsLocator = page.locator('.features_items');
