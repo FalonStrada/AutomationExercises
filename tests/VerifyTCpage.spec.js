d2de8fc43f742fdded5c6965d17c2e76ec40ae51
FalonStrada <falonstrada@gmail.com>
2024-04-16 22:43:32 -0300

    metele

--- tests/VerifyTCpage.spec.js
+++ tests/VerifyTCpage.spec.js
@@ -4,7 +4,9 @@ test("test cases tab", async ({ page }) => {
 
 await page.goto('https://automationexercise.com/');
 
-await page.getByRole('link', { name: 'Test Cases',exact:true}).click();
+const headerVisible = await page.isVisible('.container');
+//await expect(page.locator('.container > h1')).toBeVisible(); NO PUDE LOGRAR VALIDAR EL TITULOOOOO!!!
+await page.getByRole('link', { name: 'Test Cases', exact: true }).click();
 
 
 
